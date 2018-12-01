d55fd7631a1c547b04bb24e1e86feb1745cb2836
xiefeng <xiefeng@atguigu.com>
2018-12-01 16:52:38 +0800

    删除User

--- springboot-common/src/main/java/com/atguigu/springboot/bean/Student.java
+++ springboot-common/src/main/java/com/atguigu/springboot/bean/Student.java
@@ -10,14 +10,15 @@ import lombok.Data;
 @Data
 public class Student {
 
-    private String  name;
+    private String name;
 
- public String  method(User student){
+    public String method(User student) {
 
-     return "123456";
- }
+        return "123456";
+    }
 
     public static void main(String[] args) {
+        System.out.println("我在这里加了一行   会不会起冲突");
         System.out.println("大家好才是真的好");
     }
 
