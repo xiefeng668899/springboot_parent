421b3779abb0bde4c2b1f8b3ff018f840370e2cd
xiefeng <xiefeng@atguigu.com>
2018-12-01 16:38:59 +0800

    提交user

---
+++ springboot-common/src/main/java/com/atguigu/springboot/bean/Student.java
@@ -0,0 +1,21 @@
+package com.atguigu.springboot.bean;
+
+
+import lombok.Data;
+
+/**
+ * Created by acer on 2018/11/25.
+ */
+
+@Data
+public class Student {
+
+    private String  name;
+
+ public String  method(User student){
+
+     return "123456";
+ }
+
+}
+
