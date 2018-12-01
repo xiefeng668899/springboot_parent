e164e6fe90ed44b07f0603197c784452c219b6e9
xiefeng <xiefeng@atguigu.com>
2018-12-01 17:59:15 +0800

    删除User

--- springboot-common/src/main/java/com/atguigu/springboot/bean/Student.java
+++ springboot-common/src/main/java/com/atguigu/springboot/bean/Student.java
@@ -28,8 +28,8 @@ public class Student {
         System.out.println("大家好才是这里是****我修改的*****真的好");
         System.out.println("》》》》》》》》》》》》》》》》》》》》》。");
         System.out.println("》》》》》》》》》sdfvsavf》》》》》》》》》》》》。");
-        System.out.println("》》》》》》》》》dsfsfs》》》》》》》》》》》》。");
-        System.out.println("》》》》》》》》》dsfsfs》》》》》》》》》》》》。");
+        System.out.println("》》》》》》》》》dsfsfs》》》》》sxAXC》》》》》》》。");
+        System.out.println("》》》》》》》》》dsfsfs》》》》》》》》xsxc》》》》。");
         System.out.println("》》》》》》》》》dsfsfs》》》》》》》》》》》》。");
 
     }
