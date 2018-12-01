0de59ca47fa8fcef3600be467a3684856f044529
xiefeng <xiefeng@atguigu.com>
2018-12-01 17:22:42 +0800

    提交user

--- springboot-common/src/main/java/com/atguigu/springboot/bean/Student.java
+++ springboot-common/src/main/java/com/atguigu/springboot/bean/Student.java
@@ -24,6 +24,7 @@ public class Student {
         System.out.println("》》》》》》》》》》》》》》》》》》》》》。");
         System.out.println("冲突");
         System.out.println("冲突");
+        System.out.println("冲突");
     }
 
 }
